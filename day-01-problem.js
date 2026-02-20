7d39e2d33936b7088fda40f2091f7685a1cbd4d3
programmermosharof <mosharof.dev@gmail.com>
2026-02-20 23:19:59 +0600

    Problem 2 complete

--- day-01-problem.js
+++ day-01-problem.js
@@ -1,3 +1,5 @@
+
+// ======== problem 1 ========
 let name = "Mosharof";
 let age = 20;
 let city = "Dhaka";
@@ -6,4 +8,16 @@ if(age > 18){
    console.log("Tomi adult");
 }else{
     console.log("Tomi minor"); 
+}
+
+
+
+//  ======= Problem 2 ============
+
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+for(let i = 0 ; i < fruits.length; i++){
+
+    console.log(fruits[i]);
+
 }
